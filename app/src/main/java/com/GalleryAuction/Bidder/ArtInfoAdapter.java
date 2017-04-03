7a45a9c435928bc202b1dd43743c98e484762816
GOD <sytty>
2017-04-03 11:13:50 +0900

    payment 03.31_3

--- app/src/main/java/com/GalleryAuction/Bidder/ArtInfoAdapter.java
+++ app/src/main/java/com/GalleryAuction/Bidder/ArtInfoAdapter.java
@@ -1,22 +1,11 @@
 package com.GalleryAuction.Bidder;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.geno.bill_folder.R;
@@ -24,11 +13,8 @@ import com.geno.bill_folder.R;
 public class ArtInfoAdapter extends BaseAdapter {
     // 문자열 보관 ArrayList
     private ArrayList<ArtInfoItem> listViewItemList = new ArrayList<ArtInfoItem>() ;
-    TextView tv2;
-    WebView iv1;
-    String imgUrl = "http://59.3.109.220:9998/NFCTEST/art_images/";
-    Bitmap bmImg;
-//    back task;
+    TextView tv1, tv2, tv3;
+
     public ArtInfoAdapter() {
 
     }
@@ -58,50 +44,24 @@ public class ArtInfoAdapter extends BaseAdapter {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.galleryartinfo_listview_item, parent, false);
         }
-//        task = new back();
-        iv1 = (WebView) convertView.findViewById(R.id.artinfoitem1_img);
+        tv1 = (TextView) convertView.findViewById(R.id.artinfoitem1_txt);
         tv2 = (TextView) convertView.findViewById(R.id.artinfoitem2_txt);
         ArtInfoItem artInfoItem = listViewItemList.get(position);
-        iv1.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-
-//        task.execute(artInfoItem.getIcon());
-        iv1.setBackgroundColor(0);
-        iv1.setVerticalScrollBarEnabled(false);
-        iv1.setHorizontalScrollBarEnabled(false);
-        iv1.loadUrl(artInfoItem.getIcon());
+        tv1.setText(artInfoItem.getIcon());
         tv2.setText(artInfoItem.getTitle());
+
         return convertView;
     }
 
     public void addItem(String icon, String title) {
         ArtInfoItem item = new ArtInfoItem();
+
         item.setIcon(icon);
         item.setTitle(title);
 
         listViewItemList.add(item);
     }
-//    private class back extends AsyncTask<String, Integer,Bitmap> {
-//        @Override
-//        protected Bitmap doInBackground(String... urls) {
-//            // TODO Auto-generated method stub
-//            try{
-//                URL myFileUrl = new URL(urls[0]);
-//                HttpURLConnection conn = (HttpURLConnection)myFileUrl.openConnection();
-//                conn.setDoInput(true);
-//                conn.connect();
-//
-//                InputStream is = conn.getInputStream();
-//                bmImg = BitmapFactory.decodeStream(is);
-//
-//            }catch(IOException e){
-//                e.printStackTrace();
-//            }
-//            return bmImg;
-//        }
-//        protected void onPostExecute(Bitmap img){
-//            iv1.setImageBitmap(bmImg);
-//        }
-//    }
+
     public void removeitem() {
         listViewItemList.clear();
     }
--- app/src/main/java/com/GalleryAuction/Bidder/ArtInfoTagList.java
+++ app/src/main/java/com/GalleryAuction/Bidder/ArtInfoTagList.java
@@ -14,11 +14,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.GalleryAuction.Artist.ArtistArtInformation;
 import com.geno.bill_folder.R;
 
 import org.apache.http.HttpResponse;
@@ -38,11 +36,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ArtInfoTagList extends Activity {
-//    DBHelper dbHelper;
+    //    DBHelper dbHelper;
     Bitmap bmImg;
     String imgUrl = "http://59.3.109.220:9998/NFCTEST/art_images/";
 
-    private GridView gv;
+    private ListView listView;
     private ArtInfoAdapter adapter;
     Button remove_btn;
     String artkey, arttitle, image, time_e;
@@ -60,48 +58,48 @@ public class ArtInfoTagList extends Activity {
 //        dbHelper = new DBHelper(getApplicationContext(), "MoneyBook.db", null, 1);
         adapter = new ArtInfoAdapter();
 
-        gv = (GridView) findViewById(R.id.artalbum_grid);
-        gv.setAdapter(adapter);
+        listView = (ListView) findViewById(R.id.artinfo_listview);
+        listView.setAdapter(adapter);
         String albumlist = ArtAlbumlist(userId);
 
 
         JSONObject job = null;
         JSONArray  ja = null;
         try {
-           ja = new JSONArray(albumlist);
+            ja = new JSONArray(albumlist);
 
             for (int i = 0 ; i  < ja.length(); i++){
 
                 job = (JSONObject) ja.get(i);
-            artkey = job.get("art_seq").toString();
-            arttitle = job.get("art_title").toString();
-            image = job.get("art_image").toString();
-                    time_e = job.get("art_date_e").toString();
+                artkey = job.get("art_seq").toString();
+                arttitle = job.get("art_title").toString();
+                image = job.get("art_image").toString();
+                time_e = job.get("art_date_e").toString();
                 Log.d("aauser", "" +artkey);
-                adapter.addItem(imgUrl+image, arttitle);
-
+                adapter.addItem(arttitle, time_e);
+                Log.d("aaa", imgUrl+image);
             }
-            } catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+//
             }
         });
 //    getname();
-    remove_btn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+        remove_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 //            dbHelper.deleteAll();
 //            adapter.notifyDataSetChanged();
-        }
-    });
+            }
+        });
     }
 
-//    public void getname() {
+    //    public void getname() {
 //        // 읽기가 가능하게 DB 열기
 //        SQLiteDatabase db = dbHelper.getReadableDatabase();
 //        String result = "";
@@ -113,39 +111,39 @@ public class ArtInfoTagList extends Activity {
 //        }
 //
 //    }
-private String ArtAlbumlist(String msg) {
-    if (msg == null) {
-        msg = "";
-    }
-
-    String URL = "http://59.3.109.220:9998/NFCTEST/artalbum_select.jsp";
-    DefaultHttpClient client = new DefaultHttpClient();
-    try {
-
-        HttpPost post = new HttpPost(URL + "?msg=" + msg);
-
-        HttpParams params = client.getParams();
-        HttpConnectionParams.setConnectionTimeout(params, 3000);
-        HttpConnectionParams.setSoTimeout(params, 3000);
-        HttpResponse response = client.execute(post);
-        BufferedReader bufreader = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent(),
-                        "utf-8"));
-
-        String line = null;
-        String result = "";
-
-        while ((line = bufreader.readLine()) != null) {
-            result += line;
+    private String ArtAlbumlist(String msg) {
+        if (msg == null) {
+            msg = "";
         }
 
-        return result;
+        String URL = "http://59.3.109.220:9998/NFCTEST/artalbum_select.jsp";
+        DefaultHttpClient client = new DefaultHttpClient();
+        try {
 
-    } catch (Exception e) {
-        e.printStackTrace();
-        return null;
+            HttpPost post = new HttpPost(URL + "?msg=" + msg);
+
+            HttpParams params = client.getParams();
+            HttpConnectionParams.setConnectionTimeout(params, 3000);
+            HttpConnectionParams.setSoTimeout(params, 3000);
+            HttpResponse response = client.execute(post);
+            BufferedReader bufreader = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent(),
+                            "utf-8"));
+
+            String line = null;
+            String result = "";
+
+            while ((line = bufreader.readLine()) != null) {
+                result += line;
+            }
+
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-}
 
 
 }
